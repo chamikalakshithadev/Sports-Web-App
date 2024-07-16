51ff1e48c92b2a9257b63135c1d55b983afaca30
Chamika1016 <rclurathnaweera@std.appsc.sab.ac.lk>
2024-07-16 19:22:14 +0530

    adding images and relavent things

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,12 @@
 import React from "react";
+import Home from "./components/Home";
 
 const page = () => {
-  return <div className="text-9xl text-red-400">HomePage</div>;
+  return (
+    <div className="bg-[#111111] overflow-hidden">
+      <Home />
+    </div>
+  );
 };
 
 export default page;
