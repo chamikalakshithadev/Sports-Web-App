75ac17e2de707faefe3e41868a1e8e7bfa4d505e
Chamika1016 <rclurathnaweera@std.appsc.sab.ac.lk>
2024-07-16 22:14:00 +0530

    adding hero section

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,11 @@
 import React from "react";
 import Home from "./components/Home";
+import Hero from "./components/Hero/Hero";
 
 const page = () => {
   return (
     <div className="bg-[#111111] overflow-hidden">
-      <Home />
+      <Hero />
     </div>
   );
 };
